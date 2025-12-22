9e5a267f7a2d7ed572d865e8f46cb35a8e68ac3f
kcvelaga <kcvelaga@gmail.com>
2025-12-22 18:02:20 +0530

    ns current

---
+++ queries/generate/content/namespace_pages_current.sql
@@ -0,0 +1,26 @@
+SELECT
+  CURDATE() AS snapshot_date,
+  'tewiki' AS wiki_db,
+  CASE p.page_namespace
+    WHEN 0 THEN 'main'
+    WHEN 6 THEN 'file'
+    WHEN 10 THEN 'template'
+    WHEN 14 THEN 'category'
+    WHEN 118 THEN 'draft'
+  END AS ns,
+  COUNT(*) AS page_count
+FROM 
+	page p
+WHERE 
+	p.page_namespace IN (0, 6, 10, 14, 118)
+GROUP BY 
+	p.page_namespace
+ORDER BY
+  CASE p.page_namespace
+    WHEN 0 THEN 1
+    WHEN 6 THEN 2
+    WHEN 10 THEN 3
+    WHEN 14 THEN 4
+    WHEN 118 THEN 5
+  END
+ ;
