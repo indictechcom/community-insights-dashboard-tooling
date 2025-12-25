962da2c6a6a73580485cb108744975a34b65f888
kcvelaga <kcvelaga@gmail.com>
2025-12-25 17:07:12 +0530

    update new editor activation

--- queries/generate/contributors/new_editor_activation_monthly.sql
+++ queries/generate/contributors/new_editor_activation_monthly.sql
@@ -1,40 +1,41 @@
-WITH cohort_users AS (
+WITH date_params AS (
+  SELECT
+    DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), '%Y%m01000000') AS month_start,
+    DATE_FORMAT(CURDATE(), '%Y%m01000000') AS month_end
+),
+cohort_users AS (
   SELECT
     u.user_id,
-    STR_TO_DATE(CAST(u.user_registration AS CHAR), '%Y%m%d%H%i%S') AS reg_dt
+    u.user_registration
   FROM
-    user u
+    user u,
+    date_params dp
   WHERE
     u.user_registration IS NOT NULL
-    AND STR_TO_DATE(CAST(u.user_registration AS CHAR), '%Y%m%d%H%i%S') >= DATE_SUB(DATE_FORMAT(CURDATE(), '%Y-%m-01'), INTERVAL 1 MONTH)
-    AND STR_TO_DATE(CAST(u.user_registration AS CHAR), '%Y%m%d%H%i%S') < DATE_FORMAT(CURDATE(), '%Y-%m-01')
+    AND u.user_registration >= dp.month_start
+    AND u.user_registration < dp.month_end
 ),
 user_edits_24h AS (
   SELECT
     cu.user_id,
-    SUM(
-      r.rev_timestamp >= cu.reg_dt
-      AND r.rev_timestamp < cu.reg_dt + INTERVAL 1 DAY
-      AND p.page_namespace = 0
-    ) AS edits_24h_ns0
+    COUNT(CASE WHEN p.page_namespace = 0 THEN 1 END) AS edits_24h_ns0
   FROM
     cohort_users cu
   LEFT JOIN
-    actor a
-    ON a.actor_user = cu.user_id
+    actor a ON a.actor_user = cu.user_id
   LEFT JOIN
-    revision r
-    ON r.rev_actor = a.actor_id
+    revision r ON r.rev_actor = a.actor_id
+      AND r.rev_timestamp >= cu.user_registration
+      AND r.rev_timestamp < DATE_ADD(cu.user_registration, INTERVAL 1 DAY)
   LEFT JOIN
-    page p
-    ON p.page_id = r.rev_page
+    page p ON p.page_id = r.rev_page
   GROUP BY
     cu.user_id
 )
 SELECT
   CURDATE() AS snapshot_date,
   {DATABASE} AS wiki_db,
-  DATE_SUB(DATE_FORMAT(CURDATE(), '%Y-%m-01'), INTERVAL 1 MONTH) AS month,
+  DATE_FORMAT(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), '%Y-%m-01') AS month,
   COUNT(*) AS total_new_user_count,
   SUM(edits_24h_ns0 >= 1) AS activated_editor_count_1e,
   SUM(edits_24h_ns0 >= 5) AS activated_editor_count_5e,
