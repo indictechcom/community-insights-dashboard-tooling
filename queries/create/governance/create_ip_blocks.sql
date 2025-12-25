f80270ae43e87c50ea31e989c31ac74bebec8749
kcvelaga <kcvelaga@gmail.com>
2025-12-25 15:22:12 +0530

    scripts for ip blocks

---
+++ queries/create/governance/create_ip_blocks.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS ip_blocks_current (
+    `snapshot_date`         DATE            ,
+    `wiki_db`               VARCHAR(255)    ,
+    `blocked_ip`            VARCHAR(255)    ,
+    `blocked_by_user`       VARCHAR(255)    ,
+    `block_start_date`      DATE            ,
+    `expiry`                VARCHAR(255)    ,
+    PRIMARY KEY (`wiki_db`, `blocked_ip`, `block_start_date`, `snapshot_date`)
+)
+ENGINE=InnoDB
+;
