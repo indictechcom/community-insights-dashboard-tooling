356399d90726ded8c73224e561b3c4ca28ccece3
kcvelaga <kcvelaga@gmail.com>
2026-01-03 18:31:47 +0530

    cleanup

--- archive/queries/Number_of_deleted_pages_and_edits.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    DATE(ar_timestamp) AS Deletion_Date,             -- Extracts the date for dashboard filtering
-    COUNT(ar_id) AS deleted_pages                    -- Counts the number of deleted pages per day
-FROM
-    archive                                          -- Table containing records of deleted pages
-GROUP BY
-    Deletion_Date                                    -- Groups the counts by day
-ORDER BY
-    Deletion_Date;                                   -- Sorts the results by date
